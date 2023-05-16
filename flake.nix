8d2f4d4d1b3e1532b134e32c37eba9a2e13ede5e
Kelvin Steiner <me@steinerkelvin.dev>
2023-05-16 13:03:13 -0300

    init

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "An Elixir development environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in
+  {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        elixir_1_14
+        rebar
+        rebar3
+        git
+        inotify-tools
+      ];
+
+      shellHook = ''
+        # Hex and rebar setup
+        mix local.hex --if-missing --force
+        mix local.rebar --if-missing --force
+
+        # # Set Erlang cookie
+        # echo "Setting Erlang cookie"
+        # echo 'your_cookie' > ~/.erlang.cookie
+        # chmod 400 ~/.erlang.cookie
+      '';
+    };
+  };
+}
